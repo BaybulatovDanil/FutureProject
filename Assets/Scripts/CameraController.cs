3bdb032d18af70968f694fc12f00e5a294200811
Danil-Baybulatov <76666187+Danil-Baybulatov@users.noreply.github.com>
2024-03-08 10:16:39 +0300

    camera test
    add camera offset settings

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Vector3 camOffset = new Vector3(1.8f, 8.7f, -2.96f);
-    private Transform target;
+    [Header("Traget")]
+    public Transform target;
+
+    [Header("CameraOffset")]
+    public float distance = 5f;
+    public Vector3 camOffset;
+    public float minDistance = 1f;
+    public float maxDistance = 7f;
+
+    [Header("CameraSpeed")]
+    public float smoothSpeed = 5f;
+    public float scrollSensitivity = 1;
 
     private void Start()
     {
-        target = GameObject.Find("Character").transform;
+        
     }
 
     private void LateUpdate()
     {
-        this.transform.position = target.TransformPoint(camOffset);
-        this.transform.LookAt(target);
+        if (!target) 
+        {
+            print("Set target for the camera!");
+            return;
+        }
+
+        float num = Input.GetAxis("Mouse ScrollWheel");
+        distance -= num * scrollSensitivity;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+        Vector3 pos = target.position + camOffset;
+        pos -= transform.forward * distance;
+
+        transform.position = Vector3.Lerp(transform.position, pos, smoothSpeed * Time.deltaTime);
     }
+
 
 }
