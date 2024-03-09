e3c80b5c3fad55bb52b997d7f00b891fa2e06cbc
Danil-Baybulatov <baybulatov.danil@gmail.com>
2024-03-09 10:56:10 +0300

    animation
    add idle and run animation to character

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public GameObject Character;
+
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
 
     private float vInput;
     private float hInput;
+
+    private Animator playerAnimator;
+
+    private void Start()
+    {
+
+        playerAnimator = Character.GetComponent<Animator>();
+
+    }
 
     private void Update()
     {
@@ -17,6 +28,17 @@ public class CharacterMovement : MonoBehaviour
 
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+
+        if (vInput != 0)
+        {
+            playerAnimator.SetInteger("Move", 0);
+        }
+        else
+        {
+            playerAnimator.SetInteger("Move", 1);
+        }
+
     }
+
 }
 
