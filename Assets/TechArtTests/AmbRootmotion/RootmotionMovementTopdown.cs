0250d90e227f375d3dab05d301610b00f81b37b2
catstuckinowl <catstuckinowl@gmail.com>
2024-03-25 15:56:48 +0500

    Грязно собранный прототип рутмоушн движения с поворотом под топдаун камеру

--- Assets/TechArtTests/AmbRootmotion/RootmotionMovementTopdown.cs
+++ Assets/TechArtTests/AmbRootmotion/RootmotionMovementTopdown.cs
@@ -45,6 +45,37 @@ public class RootmotionMovementTopdown : MonoBehaviour
     Vector3 angleB = Vector3.zero;
 #endif
 
+    #region AnimThresholds
+    const float rot90Degrees = 0f;
+    const float rot180Degrees = -0.7f;
+    #endregion
+
+    #region AnimBools
+    bool shouldResetAnimBools = false;
+
+    //bool rotate90L = false;
+    //bool rotate90R = false;
+    //bool rotate180L = false;
+    //bool rotate180R = false;
+
+    public bool isTurning = false;
+    #endregion
+
+    void ResetAnimBools()
+    {
+        //rotate90L = false;
+        //rotate90R = false;
+        //rotate180L = false;
+        //rotate180R = false;
+        animator.ResetTrigger("TurnLeft180");
+        animator.ResetTrigger("TurnRight180");
+    }
+
+    public void EndTurn()
+    {
+        isTurning = false;
+    }
+
     void Awake()
     {
         unit = animator.gameObject;
@@ -59,14 +90,16 @@ public class RootmotionMovementTopdown : MonoBehaviour
 
     void Update()
     {
+        if (shouldResetAnimBools) ResetAnimBools();
+
         inputVector = Vector2.MoveTowards(lastInputVector, movement.action.ReadValue<Vector2>(), movementDelta * Time.deltaTime);
 
         magnitude = Mathf.Min(inputVector.magnitude, 1f);
 
-        lookVector = lookDirection.action.ReadValue<Vector2>();
+        /*lookVector = lookDirection.action.ReadValue<Vector2>();
         lastLookAngle = Mathf.SmoothStep(lastLookAngle, Vector2.Dot(lookVector,Vector2.right), 0.5f);
         //lastLookAngle = Mathf.MoveTowardsAngle(lastLookAngle, Vector2.Dot(lookVector, Vector2.right), 0.5f);
-        animator.SetFloat("LookAngle", lastLookAngle);
+        animator.SetFloat("LookAngle", lastLookAngle);*/
         dir = inputVector.normalized;
         if (hasCamera)
         {
@@ -74,8 +107,8 @@ public class RootmotionMovementTopdown : MonoBehaviour
             v3Inputs =  this.transform.InverseTransformDirection(cameraTransform.TransformDirection(v3Inputs));
             dir = new Vector2(v3Inputs.x, v3Inputs.z).normalized;
         }
-        lookAngleDotDiff = Vector3.Dot(new Vector3(dir.x, 0f, dir.y), this.transform.forward);
-        Debug.Log($"AngleDiff:[{(lookAngleDotDiff>-0.7f?(lookAngleDotDiff > 0f ? $"<color=green>{lookAngleDotDiff}</color>": $"<color=blue>{lookAngleDotDiff}</color>"): ($" <color=red>{lookAngleDotDiff}</color>"))}]");
+        lookAngleDotDiff = Vector3.Dot(this.transform.TransformDirection(new Vector3(dir.x, 0f, dir.y)), this.transform.forward);
+        //Debug.Log($"AngleDiff:[{(lookAngleDotDiff> rot180Degrees ? (lookAngleDotDiff > rot90Degrees ? $"<color=green>{lookAngleDotDiff}</color>": $"<color=blue>{lookAngleDotDiff}</color>"): ($" <color=red>{lookAngleDotDiff}</color>"))}]");
 #if UNITY_EDITOR
         angleA = this.transform.forward;
         angleB = new Vector3(dir.x, 0f, dir.y);
@@ -88,11 +121,42 @@ public class RootmotionMovementTopdown : MonoBehaviour
         lastAngle = Vector2.SignedAngle(Vector2.Lerp(lastDirection, dir, 0.1f), Vector2.up) / 180f;
         animator.SetFloat("Angle", lastAngle);
 
-        float lerpedAngleSpeedSpeed = magnitude * angleSpeed * lastLookAngle;
+        var lookAngleDotRight = Vector3.Dot(transform.right, this.transform.TransformDirection(angleB));
+        var lookInput = (lookAngleDotDiff < rot180Degrees) ? 0f : lookAngleDotRight;
+        if ((lookAngleDotDiff < rot180Degrees)/* && !isTurning*/)
+        {
+            isTurning = true;
+            if (lookAngleDotRight < 0f)
+            {
+                animator.SetTrigger("TurnLeft180");
+                shouldResetAnimBools = true;
+            }
+            else
+            {
+                animator.SetTrigger("TurnRight180");
+                shouldResetAnimBools = true;
+            }
+        }
+        else
+        {
+
+        }
+        //lookVector = /*lookDirection.action.ReadValue<Vector2>();*/lookAngleDotRight;
+        //lastLookAngle = Mathf.SmoothStep(lastLookAngle, /*Vector2.Dot(lookVector, Vector2.right)*/lookInput, 0.5f);
+        lastLookAngle = lookInput;
+        //lastLookAngle = Mathf.MoveTowardsAngle(lastLookAngle, Vector2.Dot(lookVector, Vector2.right), 0.5f);
+        animator.SetFloat("LookAngle", lastLookAngle);
+            //animator.SetFloat("Angle", lastLookAngle);
+
+
+        float lerpedAngleSpeedSpeed = magnitude * angleSpeed * lastLookAngle * 300f;
         unit.transform.localRotation *= Quaternion.Euler(0f, lerpedAngleSpeedSpeed, 0f);
         lastDirection = dir;
         lastInputVector = inputVector;
+            //Debug.Log($"LookInput:[{lookInput}], lerpedAngleSpeedSpeed:[{lerpedAngleSpeedSpeed}]");
     }
+
+
 
 #if UNITY_EDITOR
     private void OnDrawGizmos()
@@ -100,8 +164,8 @@ public class RootmotionMovementTopdown : MonoBehaviour
         var origCol = Gizmos.color;
         Gizmos.color = Color.white;
         Gizmos.DrawLine(this.transform.position + Vector3.up, this.transform.position + Vector3.up + angleA * 5f);
-        Gizmos.color = (lookAngleDotDiff > -0.7f ? (lookAngleDotDiff > 0f ? Color.green : Color.blue) : Color.red);
-        Gizmos.DrawLine(this.transform.position + Vector3.up, this.transform.position + Vector3.up + angleB * 5f);
+        Gizmos.color = (lookAngleDotDiff > rot180Degrees ? (lookAngleDotDiff > rot90Degrees ? Color.green : Color.blue) : Color.red);
+        Gizmos.DrawLine(this.transform.position + Vector3.up, this.transform.position + Vector3.up + this.transform.TransformDirection(angleB) * 5f);
     }
 #endif
 }
